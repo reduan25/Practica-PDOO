265f88056b63f21798f4b1444dd52598997bc65b
Usuario <Usuario@DESKTOP-LBS01S9>
2024-10-16 22:43:29 +0200

    Clase Coche - Getter y Setters. Closes #3

--- CarBuy/src/carbuy/CarBuy.java
+++ CarBuy/src/carbuy/CarBuy.java
@@ -23,4 +23,5 @@ public class CarBuy {
         System.out.println(c3);
     }
     
+    
 }
--- CarBuy/src/carbuy/Coche.java
+++ CarBuy/src/carbuy/Coche.java
@@ -29,6 +29,38 @@ public class Coche {
         this(otro.marca, otro.modelo, otro.year, otro.precio);
     }
     
+    public String get_Marca(){
+        return this.marca;
+    }
+    
+    public void set_Marca(String dato_marca){
+        this.marca = dato_marca;
+    }
+    
+    public String get_Modelo(){
+        return this.modelo;
+    }
+    
+    public void set_Modelo(String dato_modelo){
+        this.modelo = dato_modelo;
+    }
+    
+    public int get_Year(){
+        return this.year;
+    }
+    
+    public void set_Year(int dato_year){
+        this.year = dato_year;
+    }
+    
+    public double get_Precio(){
+        return this.precio;
+    }
+    
+    public void set_Precio(double dato_precio){
+        this.precio = dato_precio;
+    }
+    
     @Override
     public String toString() {
         return "Coche{" + "marca=" + marca + ", modelo=" + modelo + ", year=" + year + ", precio=" + precio + '}';
