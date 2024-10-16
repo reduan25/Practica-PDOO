9b60c45b3c1e0178a79658bec4fc16d270523dd9
Usuario <Usuario@DESKTOP-LBS01S9>
2024-10-16 22:21:59 +0200

    Creación de coche, y constructores. Closes #2

--- CarBuy/src/carbuy/Coche.java
+++ CarBuy/src/carbuy/Coche.java
@@ -13,4 +13,20 @@ public class Coche {
     private String modelo;
     private int year;
     private double precio;
+    
+    public Coche(String dato_marca, String dato_modelo, int dato_year, double dato_precio){
+    this.marca = dato_marca;
+    this.modelo = dato_modelo;
+    this.year = dato_year;
+    this.precio = dato_precio;
+    }
+
+    public Coche(){
+        this("Default", "Default", 2013, 16340.0);
+    }
+
+    public Coche(Coche otro){
+        this(otro.marca, otro.modelo, otro.year, otro.precio);
+    }
 }
+
