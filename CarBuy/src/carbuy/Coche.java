a32482e4a89a0dd77565b83131536f37bf280f99
Usuario <Usuario@DESKTOP-LBS01S9>
2024-10-16 22:04:14 +0200

    Creación de coche, y sus atributos de instancias y clase. Closes #2

---
+++ CarBuy/src/carbuy/Coche.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carbuy;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Coche {
+    private String marca;
+    private String modelo;
+    private int year;
+    private double precio;
+}
